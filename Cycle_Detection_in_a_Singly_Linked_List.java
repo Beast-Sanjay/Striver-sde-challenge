4e8a34286659d440a76f0d6abdcbf5fce3b0fcec
Beast-Sanjay <sanjuniky2002@gmail.com>
2023-06-30 21:41:37 +0530

    Cycle_Detection_in_a_Singly_Linked_List  solved

---
+++ Cycle_Detection_in_a_Singly_Linked_List.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+ class Node {
+     public int data;
+     public Node next;
+    
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+    
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+    
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+
+
+public class Cycle_Detection_in_a_Singly_Linked_List {
+   
+
+    public static boolean detectCycle(Node head) {
+         Node slow = head;
+    Node fast = head;
+
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow == fast) {
+            return true; 
+        }
+    }
+
+    return false;
+
+    }
+}
